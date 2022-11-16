9f21c82d27027ac94ca16ce2bd99b80dd1b83bf5
dppdd <demiro.jah@gmail.com>
2022-11-16 22:48:18 +0200

    Clean scripts

--- simple-docker-swarm-vagrant/docker-setup.sh
+++ simple-docker-swarm-vagrant/docker-setup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-echo "* Add Docker repository ..."
+echo "-- Add Docker repository"
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-echo "* Install Docker ..."
+echo "-- Install Docker"
 dnf install -y docker-ce docker-ce-cli containerd.io
 
-echo "* Enable and start Docker ..."
+echo "-- Enable and start Docker"
 systemctl enable docker
 systemctl start docker
 
-echo "* Add vagrant user to docker group ..."
+echo "-- Add vagrant user to docker group"
 usermod -aG docker vagrant
--- simple-docker-swarm-vagrant/swarm-manager-setup.sh
+++ simple-docker-swarm-vagrant/swarm-manager-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-echo "* Initiate Swarm"
+echo "-- Initiate Swarm"
 docker swarm init --advertise-addr 192.168.99.101
 
-echo "Output the token to vagrant host"
+echo "--  Output the token to file on vagrant host"
 docker swarm join-token -q worker > /vagrant/worker-token.temp
 
 
